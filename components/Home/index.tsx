b5d66a46fffa7c169cd8b611555385597406e30a
Nathan <space.man0318@gmail.com>
2024-04-25 23:46:18 +1000

    Refactor Home component

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,27 +1,15 @@
 'use client'
 
-import { useEffect } from 'react'
 import { Stack, Box, Title, Text, Button } from '@mantine/core'
-import useContracts from '@/hooks/useContracts'
 
 export default function Home() {
-  const contracts = useContracts()
-
-  const testContract = async () => {
-    if (contracts) {
-      contracts.token.name().then(console.log)
-    }
-  }
-
-  useEffect(() => {
-    testContract()
-  }, [contracts])
-
   return (
     <>
       <Stack>
         <Title>Join to earn 8% bonus</Title>
-        <Text>A new payment way to make triple wins for business based on the unique referral system.</Text>
+        <Text>
+          A new payment way to make triple wins for business based on the unique referral system.
+        </Text>
         <Box>
           <Button>Get Started</Button>
         </Box>
--- hooks/useContracts.ts
+++ hooks/useContracts.ts
@@ -2,17 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { useWallets } from '@privy-io/react-auth'
-import {
-  getToken,
-  getAdminPanel,
-  getFeePool,
-  getNftManager,
-  getPaymentEscrow,
-  getPointPool,
-  getShareToken,
-  getVault,
-  type Contracts,
-} from '@/contracts'
+import { getToken, type Contracts } from '@/contracts'
 
 export default function useContracts() {
   const [contracts, setContracts] = useState<Contracts>()
@@ -24,23 +14,9 @@ export default function useContracts() {
     const provider = await wallet.getEthersProvider()
     const signer = await provider.getSigner()
     const token = getToken(signer)
-    const adminPanel = getAdminPanel(signer)
-    const vault = getVault(signer)
-    const shareToken = getShareToken(signer)
-    const nftManager = getNftManager(signer)
-    const paymentEscrow = getPaymentEscrow(signer)
-    const pointPool = getPointPool(signer)
-    const feePool = getFeePool(signer)
 
     setContracts({
       token,
-      adminPanel,
-      vault,
-      shareToken,
-      nftManager,
-      paymentEscrow,
-      pointPool,
-      feePool,
     })
   }
 
