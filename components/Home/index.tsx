ab21c370cf207042978bf4bfa15a16fd77c2ffdf
Wayne Zhang <wayne265265@gmail.com>
2024-05-01 14:41:33 +0800

    Add level section

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,5 +1,7 @@
 'use client'
+import { useState } from 'react'
 import { useMantineTheme } from '@mantine/core'
+import _ from 'lodash'
 import {
   Container,
   Stack,
@@ -11,13 +13,77 @@ import {
   Avatar,
   Card,
   Flex,
+  Progress,
 } from '@mantine/core'
 // icons
 import { IoWallet } from 'react-icons/io5'
 import { LiaHistorySolid } from 'react-icons/lia'
+import BronzeIcon from '@/components/share/icon/BronzeIcon'
+import SilverIcon from '@/components/share/icon/SilverIcon'
+import GoldIcon from '@/components/share/icon/GoldIcon'
+import PlatinumIcon from '@/components/share/icon/PlatinumIcon'
+
+const MAX_POINTS = 1000
+
+enum Level {
+  Bronze = 'Bronze',
+  Silver = 'Silver',
+  Gold = 'Gold',
+  Platinum = 'Platinum',
+}
+
+const LevelList = [
+  {
+    level: Level.Bronze,
+    points: 0,
+    title: 'Bronze',
+  },
+  {
+    level: Level.Silver,
+    points: 200,
+    title: 'Silver',
+  },
+  {
+    level: Level.Gold,
+    points: 600,
+    title: 'Gold',
+  },
+  {
+    level: Level.Platinum,
+    points: 1000,
+    title: 'Platinum',
+  },
+]
+
+function LevelIcon(props: { level: Level; isCurrentLevel: boolean; reached: boolean }) {
+  const { level, isCurrentLevel = false, reached = false } = props
+
+  let color = null
+  if (isCurrentLevel) {
+    color = '#FFA41B'
+  } else if (reached) {
+    color = '#FFDBA4'
+  } else {
+    color = '#CBD5E1'
+  }
+
+  switch (level) {
+    case Level.Gold:
+      return <GoldIcon color={color} />
+    case Level.Silver:
+      return <SilverIcon color={color} />
+    case Level.Platinum:
+      return <PlatinumIcon color={color} />
+    case Level.Bronze:
+    default:
+      return <BronzeIcon color={color} />
+  }
+}
 
 export default function Home() {
   const theme = useMantineTheme()
+
+  const [points, setPoints] = useState(850)
 
   const renderBalance = () => {
     return (
@@ -50,6 +116,59 @@ export default function Home() {
             </Stack>
           </Flex>
         </Group>
+      </Card>
+    )
+  }
+
+  const renderLevel = () => {
+    function checkCurrentLevel(level: Level, index: number): boolean {
+      const [levelInfo] = _.filter(LevelList, el => el.level === level)
+      const nextLevel = index + 1 > LevelList.length ? null : LevelList[index + 1]
+
+      if (points >= levelInfo?.points) {
+        if (_.isEmpty(nextLevel)) {
+          return true
+        } else if (points <= nextLevel.points) {
+          return true
+        } else {
+          return false
+        }
+      }
+      return false
+    }
+
+    return (
+      <Card mt={12} shadow="sm" padding="md" radius="xs" withBorder>
+        <Group mb={18} justify="space-between">
+          <Text mt={1} fz={16} fw={700} c="#1F2937" ff="Open Sans">
+            History
+          </Text>
+          <Button style={{ width: '100px' }} variant="filled" radius="xl" c="#2C98FF" bg="#EAF5FF">
+            Detail
+          </Button>
+        </Group>
+        <Group mb={10} justify="space-between">
+          {_.map(LevelList, (el, index) => {
+            const isCurrentLevel = checkCurrentLevel(el?.level, index) || false
+            return (
+              <Stack align="center" justify="center" gap={8}>
+                <LevelIcon
+                  level={el?.level}
+                  isCurrentLevel={isCurrentLevel}
+                  reached={points > el?.points}
+                />
+                <Text fz={12} fw={isCurrentLevel ? 700 : 400}>
+                  {el?.title}
+                </Text>
+              </Stack>
+            )
+          })}
+        </Group>
+        <Progress color="yellow" radius="xl" value={_.round((points * 100) / MAX_POINTS, 0)} />
+        <Text mt={10} fz={12} fw={400} c="#1F2937" ff="Open Sans">
+          Gain more {MAX_POINTS.toLocaleString()} and wait until <b>31 Dec 2023</b> to reach
+          platinum
+        </Text>
       </Card>
     )
   }
@@ -90,6 +209,9 @@ export default function Home() {
         </Group>
         {/* balance */}
         {renderBalance()}
+
+        {/* level */}
+        {renderLevel()}
 
         <Text>
           A new payment way to make triple wins for business based on the unique referral system.
