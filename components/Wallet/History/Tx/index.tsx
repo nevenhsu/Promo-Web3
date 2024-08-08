359864621ba7132aa9263f31c73d85d06b2ce979
Neven <space.man0318@gmail.com>
2024-08-08 19:02:46 +1000

    Update tx history error

--- components/Wallet/History/Tx/index.tsx
+++ components/Wallet/History/Tx/index.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 import { useAsyncFn } from 'react-use'
 import Image from 'next/image'
 import { Paper, Stack, Group, Space, Box, Divider, Skeleton } from '@mantine/core'
-import { Title, Text, CopyButton, ActionIcon, Pill } from '@mantine/core'
+import { Title, Text, CopyButton, ActionIcon, Pill, Center } from '@mantine/core'
 import RwdLayout from '@/components/share/RwdLayout'
 import { getUserTransaction } from '@/services/transaction'
 import { getToken } from '@/contracts/tokens'
@@ -40,87 +40,95 @@ export default function HistoryTx({ tx }: { tx: string }) {
 
         <Space h={40} />
 
-        <Skeleton visible={loading}>
+        {error ? (
           <Paper radius="md" p="md" shadow="xs">
-            <Stack>
-              <Group justify="space-between">
-                <Text fz="sm" c="dark.4">
-                  Status
-                </Text>
-                <Text fz="sm" fw={500} c={label.color}>
-                  {label.text}
-                </Text>
-              </Group>
-
-              <Group justify="space-between" align="start">
-                <Text fz="sm" c="dark.4">
-                  Network
-                </Text>
-                <Text fz="sm">{network.name}</Text>
-              </Group>
-
-              <Group justify="space-between" align="start">
-                <Text fz="sm" c="dark.4">
-                  Tx Hash
-                </Text>
-                <Box w={200}>
-                  <TxLink val={value?.hash || ''} onClick={handleLink} />
-                </Box>
-              </Group>
-
-              <Group justify="space-between" align="start">
-                <Text fz="sm" c="dark.4">
-                  From
-                </Text>
-                <Box w={200}>
-                  <WalletText
-                    val={value?.from || ''}
-                    username={value?._fromWallet?._user.username}
-                  />
-                </Box>
-              </Group>
-
-              <Group justify="space-between" align="start">
-                <Text fz="sm" c="dark.4">
-                  To
-                </Text>
-                <Box w={200}>
-                  <WalletText val={value?.to || ''} username={value?._toWallet?._user.username} />
-                </Box>
-              </Group>
-
-              <Divider />
-
-              <Group justify="space-between" align="start">
-                <Text fz="sm" c="dark.4">
-                  Token
-                </Text>
-                <Group gap="xs">
-                  {token?.icon ? (
-                    <Image src={token.icon} width={18} height={18} alt={token.name} />
-                  ) : null}
-                  <Text fz="sm">{token?.symbol || 'Unknown'}</Text>
-                </Group>
-              </Group>
-
-              <Group justify="space-between" align="start">
-                <Text fz="sm" c="dark.4">
-                  Amount
-                </Text>
-                <Text fz="sm">{value?.token?.amount || 'No data'}</Text>
-              </Group>
-
-              <Group justify="space-between" align="start">
-                <Text fz="sm" c="dark.4">
-                  Date
-                </Text>
-                <Text fz="sm">
-                  {formateLocalDate(value?.createdAt || 0, 'MMM dd yyyy h:mm aa')}
-                </Text>
-              </Group>
-            </Stack>
+            <Center>
+              <Text c="red">{error.response?.data.error || 'Unknown error'}</Text>
+            </Center>
           </Paper>
-        </Skeleton>
+        ) : (
+          <Skeleton visible={loading}>
+            <Paper radius="md" p="md" shadow="xs">
+              <Stack>
+                <Group justify="space-between">
+                  <Text fz="sm" c="dark.4">
+                    Status
+                  </Text>
+                  <Text fz="sm" fw={500} c={label.color}>
+                    {label.text}
+                  </Text>
+                </Group>
+
+                <Group justify="space-between" align="start">
+                  <Text fz="sm" c="dark.4">
+                    Network
+                  </Text>
+                  <Text fz="sm">{network.name}</Text>
+                </Group>
+
+                <Group justify="space-between" align="start">
+                  <Text fz="sm" c="dark.4">
+                    Tx Hash
+                  </Text>
+                  <Box w={200}>
+                    <TxLink val={value?.hash || ''} onClick={handleLink} />
+                  </Box>
+                </Group>
+
+                <Group justify="space-between" align="start">
+                  <Text fz="sm" c="dark.4">
+                    From
+                  </Text>
+                  <Box w={200}>
+                    <WalletText
+                      val={value?.from || ''}
+                      username={value?._fromWallet?._user.username}
+                    />
+                  </Box>
+                </Group>
+
+                <Group justify="space-between" align="start">
+                  <Text fz="sm" c="dark.4">
+                    To
+                  </Text>
+                  <Box w={200}>
+                    <WalletText val={value?.to || ''} username={value?._toWallet?._user.username} />
+                  </Box>
+                </Group>
+
+                <Divider />
+
+                <Group justify="space-between" align="start">
+                  <Text fz="sm" c="dark.4">
+                    Token
+                  </Text>
+                  <Group gap="xs">
+                    {token?.icon ? (
+                      <Image src={token.icon} width={18} height={18} alt={token.name} />
+                    ) : null}
+                    <Text fz="sm">{token?.symbol || 'Unknown'}</Text>
+                  </Group>
+                </Group>
+
+                <Group justify="space-between" align="start">
+                  <Text fz="sm" c="dark.4">
+                    Amount
+                  </Text>
+                  <Text fz="sm">{value?.token?.amount || 'No data'}</Text>
+                </Group>
+
+                <Group justify="space-between" align="start">
+                  <Text fz="sm" c="dark.4">
+                    Date
+                  </Text>
+                  <Text fz="sm">
+                    {formateLocalDate(value?.createdAt || 0, 'MMM dd yyyy h:mm aa')}
+                  </Text>
+                </Group>
+              </Stack>
+            </Paper>
+          </Skeleton>
+        )}
       </RwdLayout>
 
       <Space h={100} />
@@ -135,7 +143,7 @@ function WalletText({ val, username }: { val: string; username?: string }) {
         {({ copied, copy }) => (
           <Group wrap="nowrap" align="start" gap="xs" justify="space-between">
             <Box>
-              {username ? <Pill fz="xs" mb={2}>{`@${username}`}</Pill> : null}
+              {username ? <Pill size="sm" mb={2}>{`@${username}`}</Pill> : null}
               <Text fz="xs" c={copied ? 'green' : 'black'} style={{ wordBreak: 'break-word' }}>
                 {username ? formatAddress(val) : val}
               </Text>
